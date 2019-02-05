f44dd3f74af9bc49ae002dbddf4dd562f5feaafc
Blaked <dorian.becker@gadz.org>
2019-02-05 10:22:28 +0100

    init 🎉

---
+++ lib/fast_jsonapi_devour.rb
@@ -0,0 +1,2 @@
+require "fast_jsonapi_devour/version"
+require "fast_jsonapi_devour/model"
---
+++ lib/fast_jsonapi_devour/model.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'active_support'
+
+module FastJsonapiDevour
+  module Model
+    extend ::ActiveSupport::Concern
+
+    included do
+
+      def self.devour_attributes
+        attributes_to_serialize.transform_values { |v| '' }
+      end
+
+      def self.devour_relationships
+        relationships_to_serialize.transform_values do |relationship|
+          {
+            relationship.name =>
+              {
+                jsonApi: relationship.relationship_type,
+                type: relationship.name
+              }
+          }
+        end
+      end
+
+      def self.devour_model
+        devour_attributes.merge(devour_relationships)
+      end
+    end
+  end
+end
